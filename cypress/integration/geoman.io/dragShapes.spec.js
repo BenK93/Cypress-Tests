864d4b44f44eadedda6d8b23df90d07d1fe8dd62
BenK93 <benkoren23@gmail.com>
2021-11-10 15:16:02 +0200

    Test: Dragging Polygon -- Done

--- cypress/integration/geoman.io/dragShapes.spec.js
+++ cypress/integration/geoman.io/dragShapes.spec.js
@@ -9,44 +9,29 @@ describe("Test: Dragging Shapes", () => {
   });
 
   it("Dragging Polygon", () => {
-    cy.get(
-      "#map > div.leaflet-control-container > div.leaflet-top.leaflet-left > div.leaflet-pm-toolbar.leaflet-pm-draw.leaflet-bar.leaflet-control > div:nth-child(4) > a > div"
-    ).click();
+    cy.get(".leaflet-pm-icon-polygon").click();
     cy.get("#map").click(xCoord, yCoord);
     cy.get("#map").click(250, 100);
     cy.get("#map").click(250, 200);
     cy.get("#map").click(xCoord, yCoord);
-    const polygon = cy.get(
-      "#map > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > svg > g > path"
-    );
-    cy.get(
-      "#map > div.leaflet-control-container > div.leaflet-top.leaflet-left > div.leaflet-pm-toolbar.leaflet-pm-edit.leaflet-bar.leaflet-control > div:nth-child(2) > a > div"
-    ).click();
-    cy.get("#map")
-      .trigger("mousedown", { which: 1 })
-      .trigger("mousemove", { clientX: 200, clientY: 200 })
+    cy.get(".leaflet-pm-icon-drag").click();
+    cy.wait(500);
+    cy.get(".leaflet-pm-draggable")
+      .trigger("mouseover")
+      .trigger("mousedown", { which: 1, force: true })
+      .trigger("mousemove", {
+        clientX: xCoord + 150,
+        clientY: yCoord + 150,
+        screenX: xCoord + 150,
+        screenY: yCoord + 150,
+        pageX: xCoord + 150,
+        pageY: yCoord + 150,
+      })
       .trigger("mouseup", { force: true });
-    // polygon.click();
-    // polygon.trigger("mousedown", {
-    //   which: 1,
-    //   pageX: 0,
-    //   pageY: 0,
-    // });
-    // polygon.trigger("mousemove", {
-    //   which: 1,
-    //   pageX: xCoord - 150,
-    //   pageY: yCoord - 150,
-    // });
-    // polygon.trigger("mouseup");
-    // cy.get(
-    //   "#map > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > svg > g > path"
-    // )
-    //   .invoke("d", "d", "M339 261L441 266L346 15")
-    //   .should("have.attr", "d", "M339 261L441 266L346 15");
-    returnAssertion().should("have.length", 1);
+    fatherOfShapesElement().should("have.length", 1);
   });
 
-  const returnAssertion = () => {
+  const fatherOfShapesElement = () => {
     return cy.get(
       "#map > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > svg > g"
     );
